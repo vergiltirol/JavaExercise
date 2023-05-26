0961d543694a4cbf8bffd88d0a1d5d639c3b7f98
v.tirol <v.tirol@fujitsu.com>
2023-05-26 09:49:55 +0800

    updated colorgame mechanics

--- src/random_number_generator/ColorGame.java
+++ src/random_number_generator/ColorGame.java
@@ -28,57 +28,37 @@ public class ColorGame {
 		Random random = new Random();
 		Scanner scan = new Scanner(System.in);
 		boolean cont = false;
-		int money = 35;
+
 		do {
 			showColorMenu();
-			int perBet = 5;
-			int winningRate = 3;
 
 			List<Integer> userInput = new ArrayList<>();
-			
+			List<Integer> winningColors = new ArrayList<>();
+
 			boolean addMoreColor = false;
 			do {
 				System.out.println("Enter bet color: ");
 				String color = scan.next();
-				
+
 				userInput.add(getIndexOfColors(color));
-				
+
 				System.out.println("Bet more colors? Y/N");
 				addMoreColor = scan.next().toLowerCase().equals("y") ? true : false;
-			}while(addMoreColor);
-			
-			int r = 0; //random.nextInt(6);
-			roulette(r);
+			} while (addMoreColor);
 
-			int winCount = 0;
-			for (int i : userInput) {
-				if (i == r) {
-					winCount++;
-				}
+			int r = 0;
+			for (int i = 0; i < 3; i++) {
+				r = random.nextInt(6);
+				roulette(r);
+				winningColors.add(r);
 			}
-			
-			
-			if (userInput.size() == 1) {
-				System.out.println("Your bet is: " + COLORS[userInput.get(0)].toUpperCase());
-			} else {
-				System.out.print("Your bets are: ");
-				for (int i : userInput) {
-					System.out.print(COLORS[i].toUpperCase() + " ");
-				}
-				System.out.println();
-			}
-			
-			
-			int winnings = 0;
-			if (winCount > 0) {
-				winnings = (winningRate * perBet) * winCount;
-				money += winnings;
-				System.out.format("You win! P%d \n" + "your total money: %d ", winnings, money);
-			} else {
-				winnings = (perBet * userInput.size());
-				money -= (perBet * userInput.size());
-				System.out.format("You lose! P%d \n" + "your total money: P%d ", winnings, money);
-			}
+
+			printSelectedColors(userInput);
+
+			int winCount = getWinCount(userInput, winningColors);
+
+			int initialMoney = 35;
+			int money = printSomething(winCount, userInput.size(), initialMoney);
 
 			if (money < 15) {
 				System.out.println("Out of money");
@@ -87,9 +67,6 @@ public class ColorGame {
 				System.out.println("Continue playing? Y/N");
 				cont = scan.next().toLowerCase().equals("y") ? true : false;
 			}
-			
-//			System.out.println("Continue playing? Y/N");
-//			cont = scan.next().toLowerCase().equals("y") ? true : false;
 
 		} while (cont);
 
@@ -100,32 +77,61 @@ public class ColorGame {
 	protected static void showColorMenu() {
 		System.out.println("Choose from the colors:");
 		int i = 0;
-		for(String c: COLORS) {
+		for (String c : COLORS) {
 			System.out.print("\t" + COLOR_WHEEL[i] + c.toUpperCase() + ANSI_RESET);
 			i++;
 		}
 		System.out.println();
 	}
 
-	protected static int checkSameColor(int[] n) {
-		int counter = 0;
-		for (int i = 1; i < n.length; i++) {
-			if(n[i-1] == n[i]) {
-				counter++;
+	protected static void printSelectedColors(List<Integer> userInput) {
+		if (userInput.size() == 1) {
+			System.out.println("Your bet is: " + COLORS[userInput.get(0)].toUpperCase());
+		} else {
+			System.out.print("Your bets are: ");
+			for (int i : userInput) {
+				System.out.print(COLORS[i].toUpperCase() + " ");
+			}
+			System.out.println();
+		}
+	}
+
+	protected static int printSomething(int winCount, int betCount, int money) {
+		int perBet = 5;
+		int winningRate = 3;
+		int winnings = 0;
+		if (winCount > 0) {
+			winnings = (winningRate * perBet) * winCount;
+			money += winnings;
+			System.out.format("You win! P%d \n" + "your total money: %d ", winnings, money);
+		} else {
+			winnings = (perBet * betCount);
+			money -= (perBet * betCount);
+			System.out.format("You lose! P%d \n" + "your total money: P%d ", winnings, money);
+		}
+		return money;
+	}
+
+	protected static int getWinCount(List<Integer> userInput, List<Integer> winningColors) {
+		int winCount = 0;
+		for (int i = 0; i < userInput.size(); i++) {
+			for (int j = 0; j < winningColors.size(); j++) {
+				if (userInput.get(i) == winningColors.get(j)) {
+					winCount++;
+				}
 			}
 		}
-		return counter;
+		return winCount;
 	}
 
 	protected static void roulette(int r) {
 		System.out.println("WINNING COLOR IS: " + ANSI_BOLD + COLOR_WHEEL[r] + COLORS[r].toUpperCase() + ANSI_RESET);
 
 	}
-	
+
 	protected static int getIndexOfColors(String input) {
 		List<String> ab = Arrays.asList(COLORS);
 		return ab.indexOf(input);
 	}
-	
 
 }
